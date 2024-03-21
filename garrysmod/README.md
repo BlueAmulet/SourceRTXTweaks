845f9eb26f839315d9d44d0ffe5cca2a15f06cef
BlueAmulet <43395286+BlueAmulet@users.noreply.github.com>
2024-03-21 15:28:12 -0600

    Garry's Mod Improvements
    Add 64bit patches
    Prevent shader dlls from being loaded in the first place
    Fix menu crashing
    Remove 2 hardware light limit
        The limit is still 4, and cannot be easily raised
    Add labels to patches
    Colored logging when colorama is available
    Make patcher easier to use

--- garrysmod/README.md
+++ garrysmod/README.md
@@ -50,19 +50,37 @@ Credits to [@khang06](https://github.com/khang06) for this fix, found [here](htt
 
 ### gm_construct, gm_flatgrass, and other shader based skybox maps crash on load
 
-Go inside `garrysmod\bin` and rename `game_shader_generic_garrysmod.dll` to something else.  
+Additional patches have been added that prevent the loading of shader dlls.  
+If these maps still crash you, then follow the next instructions:
+
+Go inside `garrysmod\bin` and remove or rename `game_shader_generic_garrysmod.dll` to something else.  
 This will remove shaders for shader based skyboxes, and post processing effects which aren't used with RTX Remix anyway.  
 Note: While the maps will load now, they still lack a skybox and may or may not have a functioning sun.
 
 ## Recommendations
 
-Part of the process to using RTX Remix with GMod is to replace the menu files with ones from [here](https://github.com/robotboy655/gmod-lua-menu)  
-If you removed the `include( "loading.lua" )` line from `garrysmod\lua\menu\menu.lua`, then also go into `garrysmod\lua\menu\background.lua` and make the following change:  
-```  
+Additional patches have been added that should prevent the menu from crashing RTX Remix.  
+If the menu crashes you still, replace the menu files with ones from [here](https://github.com/robotboy655/gmod-lua-menu), then follow the next instructions:
+
+Go into `garrysmod\lua\menu\menu.lua` and remove or comment the `include( "loading.lua" )` line
+
+Go into `garrysmod\lua\menu\background.lua` and make the following change:  
+```
 local function ShouldBackgroundUpdate()
 
 	return false
 
-end  
+end
 ```  
-This will prevent an endless spam of script errors.  
+This will prevent an endless spam of script errors.
+
+Go into `garrysmod\lua\menu\custom\_errors.lua` and remove or comment the following code:  
+```
+	if ( GetConVarNumber( "mat_dxlevel" ) < 90 ) then
+		table.insert( Errors, {
+			last	= SysTime(),
+			text	= "mat_dxlevel is less than 90!"
+		} )
+	end
+```  
+This will hide the "mat_dxlevel is less than 90!" message
--- garrysmod/applypatch.py
+++ garrysmod/applypatch.py
@@ -1,37 +1,93 @@
 import sys
 import os
 
-patches = {
-'client.dll': [
-    ['558bec538b5d08568b7510578b', '32c0c3'],
-    ['00e8????efff8b0d??????10??????????????????????????ff', '01'],
+patches32 = {
+'garrysmod/bin/client.dll': [
+    ['558bec538b5d08568b7510578b', '32c0c3'], # c_frustumcull
+    ['00e8????efff8b0d??????10??????????????????????????ff', '01'], # r_forcenovis
 ],
-'engine.dll': [
-    ['558bec538b5d08568b7510578b7d0c565753e8????ffff83c40c83f8027466', '32c0c3'],
-    ['558bec538b5d08568b7510578b7d0c565753e8??ddffff83c40c83f8027453', '32c0c3'],
-    ['7530f30f10', 'eb'],
-    ['7e3ceb06', 'eb'],
-    ['75438b4604', 'eb'],
+'bin/engine.dll': [
+    ['558bec538b5d08568b7510578b7d0c565753e8????ffff83c40c83f8027466', '32c0c3'], # c_frustumcull
+    ['558bec538b5d08568b7510578b7d0c565753e8??ddffff83c40c83f8027453', '32c0c3'], # c_frustumcull
+    ['7530f30f10', 'eb'], # brush entity backfaces
+    ['7e3ceb06', 'eb'], # world backfaces
+    ['75438b4604', 'eb'], # world backfaces
 ],
-'shaderapidx9.dll': [
-    ['83c4108be55dc3cccccccccccccccccc55', '85c07502b0048be55dc3'],
+'bin/materialsystem.dll': [
+    ['558bec83ec7c57', '31c0c3'], # no shader loading
+],
+'bin/shaderapidx9.dll': [
+    ['0f4cd68bf7', '909090'], # four hardware lights
+    ['83c4108be55dc3cccccccccccccccccc55', '85c07502b0048be55dc3'], # zero sized buffer
+    ['558bec51a140200e10538b5d', 'c3'] # vertex shader constants
 ],
 }
 
-if len(sys.argv) != 2:
-    print('Usage: applypatch.py folder')
-    sys.exit(1)
-vers = sys.argv[1]
-if not os.path.isdir(vers):
-    print(f'Error: [{vers}]: No such directory')
+patches64 = {
+'bin/win64/client.dll': [
+    ['4883ec480f1022', '32c0c3'], # c_frustumcull
+    ['0fb68154', 'b001c3'], # r_forcenovis [getter]
+],
+'bin/win64/engine.dll': [
+    # TODO: Missing c_frustumcull patches
+    ['753cf30f10', 'eb'], # brush entity backfaces
+    ['7e5244', 'eb'], # world backfaces
+    ['753c498b4204', 'eb'], # world backfaces
+],
+'bin/win64/materialsystem.dll': [
+    ['40554155', '31c0c3'], # no shader loading
+],
+'bin/win64/shaderapidx9.dll': [
+    ['480f4ec1c7', '90909090'], # four hardware lights
+    ['4833cce804c5', '85c0750466b80400'], # zero sized buffer
+    ['48895c240848896c24104889742418574883ec20488b05', 'c3'] # vertex shader constants
+],
+}
+
+# Colored log output
+try:
+    import colorama
+    colorama.just_fix_windows_console()
+
+    def logwarn(*args, **kwargs):
+        print(colorama.Fore.YELLOW, end='')
+        print(*args, **kwargs)
+        print(colorama.Style.RESET_ALL, end='')
+
+    def logerror(*args, **kwargs):
+        print(colorama.Fore.RED, end='')
+        print(*args, **kwargs)
+        print(colorama.Style.RESET_ALL, end='')
+except:
+    logwarn = print
+    logerror = print
+
+if len(sys.argv) < 1 or len(sys.argv) > 2:
+    print('Usage: applypatch.py [folder]')
     sys.exit(1)
 
+# Verify arguments
+if len(sys.argv) >= 2:
+    vers = sys.argv[1]
+    if not os.path.isdir(vers):
+        logerror(f'Error: [{vers}]: No such directory')
+        sys.exit(1)
+else:
+    vers = os.getcwd()
+
+# Select patches
+if os.path.isdir(os.path.join(vers, 'bin/win64')):
+    patches = patches64
+else:
+    patches = patches32
+
+# Check if all files are present
 ver = {}
 missing = False
 for fname in patches:
     path = os.path.join(vers, fname)
     if not os.path.exists(path):
-        print(f'Error: Missing file [{fname}]')
+        logerror(f'Error: Missing file [{fname}]')
         missing = True
     else:
         print(f'Loading {fname}')
@@ -61,6 +117,9 @@ def findmask(data, hexstr, start=0):
             return findpos
         start = findpos + 1
 
+# Apply all patches
+ver_out = {}
+problems = False
 os.makedirs(os.path.join(vers, 'patched'), exist_ok=True)
 for fname in patches:
     print(f'\nPatching {fname}')
@@ -70,10 +129,24 @@ for fname in patches:
         if findpos != -1 and findpos2 == -1:
             pdata = bytes.fromhex(patch[1])
             print(f'{findpos:X}: Changing `{ver[fname][findpos:findpos+len(pdata)].hex()}` to `{patch[1]}`')
-            ver[fname][findpos:findpos+len(pdata)] = pdata
+            if fname not in ver_out:
+                ver_out[fname] = ver[fname]
+            ver_out[fname][findpos:findpos+len(pdata)] = pdata
         else:
-            print(f'Failed to locate patch for {patch[0]}')
+            logwarn(f'Failed to locate patch for {patch[0]}')
+            problems = True
 
+# Save patches
+print('\nWriting changes to "patched" folder')
 for fname in patches:
-    with open(os.path.join(vers, 'patched', fname), 'wb') as f:
-        f.write(ver[fname])
+    if fname in ver_out:
+        print(f'Writing {fname}')
+        oname = os.path.join(vers, 'patched', fname)
+        os.makedirs(os.path.dirname(oname), exist_ok=True)
+        with open(oname, 'wb') as f:
+            f.write(ver_out[fname])
+    else:
+        print(f'No patches applied to {fname}')
+
+if problems:
+    logwarn('\nWarning: Not all patches applied successfully')
